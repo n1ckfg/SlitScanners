a329e98dd88707b23bea1c93da4f61ab696299a0
Nick Fox-Gieg <nick@fox-gieg.com>
2019-04-28 23:23:19 -0400

    testing ios version

--- openFrameworks/SlitScanners_ios/src/Scanner.h
+++ openFrameworks/SlitScanners_ios/src/Scanner.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "ofMain.h"
+#include "ofxiOS.h"
 
 class Scanner {
     
--- openFrameworks/SlitScanners_ios/src/ofApp.mm
+++ openFrameworks/SlitScanners_ios/src/ofApp.mm
@@ -28,61 +28,6 @@ void ofApp::draw() {
 	gui.draw();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) { 
-
-}
-
 ofImage ofApp::wave(ofImage sourceImg) {
 	ofImage newImg;
 	newImg.clone(sourceImg);
